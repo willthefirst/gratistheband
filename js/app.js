7f878213d3b83459f67d8db734d38c5a2dbc167f
Will Moritz <will@crowdfavorite.com>
2019-11-07 15:35:38 -0500

    added basic audio

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 $(document).foundation();
+
+
+$('.welcome_audio').click(function() {
+  if (this.paused == false) {
+      this.pause();
+      alert('music paused');
+  } else {
+      this.play();
+      alert('music playing');
+  }
+});
